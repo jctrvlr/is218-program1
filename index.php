fdab6a1860fbc371b84f55e34d40987466133ded
jctrvlr <jcummings5@gmail.com>
2016-09-29 13:35:05 -0400

    added some code for class lesson1

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
+// this is how you print text in php
 echo "hello world";
+// this how you store a value in a variable
+$myvar = 'Some text that I want to store in a variable';
+// this is an example of the difference between single quotes and double quotes
+echo '<br>';
+echo '$myvar';
+echo '<br>';
+echo "$myvar";
+
 ?>
